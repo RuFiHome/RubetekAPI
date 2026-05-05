1e2d2ce5a000c2f51ea87601c9d3302f02bbf197
AlexVot <36454663+RuFiHome@users.noreply.github.com>
2026-05-05 17:35:34 +0300

    Update exceptions.py

--- SAARubetekAPI/exceptions.py
+++ SAARubetekAPI/exceptions.py
@@ -6,7 +6,7 @@ class AuthorizationRequiredRubetekAPIError(RubetekAPIError):
     def __init__(self, message):
         super().__init__("\n--------------------------------------------------------------------------\n"
                          f"{message}"
-                         "rubetek_api = RubetekAPI()\n"
+                         "rubetek_api = SAARubetekAPI()\n"
                          "phone = input('Введите номер телефона (c +7): ')"
                          "await rubetek_api.authentication(phone=phone)"
                          "code = input('Введите последние 4 цифры номера звонившего телефона: ')"
